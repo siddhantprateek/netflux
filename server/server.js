6280733eac4c072678585b845528ef7368622fb0
siddhantprateek <siddhantprateek@gmail.com>
2022-04-06 12:14:09 +0530

    added config middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();
 const PORT = process.env.PORT || 4000
 const app = express();
+dotenv.config();
 
+
+// middleware
+app.use(express.json())
 app.use(cors());
 
+// routes
 app.get('/', (req, res) => {
   res.send('server is running');
 })
