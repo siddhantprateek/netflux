1d00dea5f9f512c52dcce9122af8eccfdfc3f88d
siddhantprateek <siddhantprateek@gmail.com>
2022-04-24 18:31:44 +0530

    🚩SignUp home page auth

--- client/src/routes/login/login.routes.jsx
+++ client/src/routes/login/login.routes.jsx
@@ -1,34 +1,87 @@
-import React from 'react'
-import './login.style.css'
+import React, { useState } from "react";
+import { useUserAuth } from "../../context/context";
+import { useNavigate } from "react-router-dom";
+import "./login.style.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { LogIn, user, LogOut } = useUserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await LogIn(email, password);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await LogOut();
+      navigate("/in/login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="login">
       <div className="login-container">
         <h1>Sign In</h1>
-        <form action="">
-          <input className='sign-inp sign' type="email" placeholder='Email or phone number'/>
-          <input className='sign-inp sign' type="password" placeholder='Password'/>
-          <button className="sign-in-btn sign">Sign In</button>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            className="sign-inp sign"
+            type="email"
+            placeholder="Email or phone number"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="sign-inp sign"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button 
+            className="sign-in-btn sign"
+            type="submit"
+            >Sign In</button>
         </form>
+        {user && (
+          <>
+            <p>{user.email}</p>
+            <button 
+            className="sign-in-btn sign"
+            onClick={handleLogout}>LogOut</button>
+          </>
+          )}
+        {error && <p>{error}</p>}
         <div className="login-support">
           <div className="remember-me">
-            <input type="checkbox" />Remember me 
+            <input type="checkbox" />
+            Remember me
           </div>
           <p>Need help?</p>
         </div>
 
         <div className="other-content">
-            <p className='fb'>Login with Google</p> 
-            <p><span>New to Netflux?</span> Sign up now</p>
-            <p className='recaptcha'>
-              This page is protected by Google reCAPTCHA to
-              ensure you're not a bot. <a href="/">Learn More</a>.
-            </p>
+          <p className="fb">Login with Google</p>
+          <p>
+            <span>New to Netflux?</span> Sign up now
+          </p>
+          <p className="recaptcha">
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <a href="/">Learn More</a>.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/routes/signup/signup.routes.jsx
+++ client/src/routes/signup/signup.routes.jsx
@@ -4,7 +4,19 @@ import './signup.style.css'
 const SignUp = () => {
   return (
     <div className="signup">
-        <h1>Sign up</h1>
+      <div className="sign-up-header">
+      </div>
+      <div className="bottom-fade">
+        <h1>Ulimited movies, TV</h1>
+        <h1>shows and more.</h1>
+        <h3>Watch anywhere. Cancel anytime.</h3>
+
+        <p>Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className="input-email-field">
+          <input type="text" placeholder='Email address' />
+          <button>Get Started  &#x3009;</button>
+        </div>
+      </div>
     </div>
   )
 }
